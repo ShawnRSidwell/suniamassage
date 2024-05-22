2ea63243bbeb2693cf0e78ff0f2e9ea937779cef
ShawnRSidwell <shawnrsidwell@gmail.com>
2024-05-21 22:26:30 -0700

    change button color

--- src/ui/Button.jsx
+++ src/ui/Button.jsx
@@ -2,7 +2,7 @@ function Button({ children, onClick, className }) {
   return (
     <button
       onClick={onClick}
-      className={`px-4 py-2 bg-blue-500 text-white rounded-md focus:outline-none hover:bg-blue-600 ${className}`}
+      className={`px-4 py-2 bg-[#60A76F] text-white rounded-md focus:outline-none hover:bg-blue-600 ${className}`}
     >
       {children}
     </button>
