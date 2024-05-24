191bbfad2eb71ad34c44388c43ddcb7407e13a50
ShawnRSidwell <shawnrsidwell@gmail.com>
2024-05-24 12:30:20 -0700

    Change about section styling

--- src/ui/Hero.jsx
+++ src/ui/Hero.jsx
@@ -1,9 +1,9 @@
 function Hero({ headline, image, children }) {
   return (
-    <div className="flex flex-col items-center md:flex-row justify-center gradientbackground h-[60vh] bg-[#938ccf] md:h-[80vh] ">
+    <div className="flex flex-col items-center md:flex-row justify-center gradientbackground  md:bg-[#938ccf] md:h-[80vh] ">
       <div className="md:flex md:justify-evenly items-center text-slate-500 font-medium bg-white rounded-3xl p-3 m-3 md:p-24 md:m-6">
         <img
-          className="rounded-full shadow-lg mx-auto size-1/2 md:h-96 md:w-96 object-cover md:ml-24 border-[#756DAF]"
+          className="rounded-full size-4/5 shadow-lg mx-auto md:size-1/2 md:h-96 md:w-96 object-cover md:ml-24 border-[#756DAF]"
           src={image}
           alt="profile picture"
         />
