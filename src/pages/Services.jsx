7a6955490024cd6ed443e9de5c36e5905480888f
ShawnRSidwell <shawnrsidwell@gmail.com>
2024-05-28 12:45:30 -0700

    set up initial pricing page

--- src/pages/Services.jsx
+++ src/pages/Services.jsx
@@ -1,25 +1,37 @@
 import Box from "../ui/Box";
 import Button from "../ui/Button";
+import Headline from "../ui/Headline";
+import Panel from "../ui/Panel";
 import PricingCard from "../ui/PricingCard";
 
 function Services() {
   return (
     <section id="services">
-      <h1 className="text-[5em] font-extrabold tracking-tight text-center leading-none p-2 uppercase">
+      <h1 className="text-[4em] md:text-[5em] font-light tracking-tight text-center uppercase w-full  ">
+        <span className="font-normal tracking-[-15px] mx-3">----</span>
         Services
+        <span className="font-normal tracking-[-15px] mx-3">----</span>
       </h1>
-      <hr className="border-[#e2dac5] w-1/4 mx-auto  border-4 mb-9" />
-      <Box headline="Ayurveda Massage" className="">
+      <Box headline="Ayurvedic Massage" className="">
         <hr className="border-[#60A76F] border-2 w-1/4 my-1 mb-3" />
-        This is a full body treatment including scalp and belly. It is not going
-        to treat specific areas of tension in the body, but is rather a
-        wholistic approach. There will be ALOT of warm oil poured on your body.
-        You will be appropriately draped (breasts and gluteal cleft covered) at
-        all times but it may feel minimal. It is recommended to not wear any
-        undergarments for the ideal treatment. Of course all of this can be
-        modified to suit your comfort level.
+        <p className="text-gray-700">
+          This is a full body treatment including scalp and belly. It is not
+          going to treat specific areas of tension in the body, but is rather a
+          wholistic approach. There will be ALOT of warm oil poured on your
+          body. You will be appropriately draped (breasts and gluteal cleft
+          covered) at all times but it may feel minimal. It is recommended to
+          not wear any undergarments for the ideal treatment. Of course all of
+          this can be modified to suit your comfort level.
+        </p>
+        <div className="flex items-baseline text-gray-900 ">
+          <span className="text-3xl font-semibold">$</span>
+          <span className="text-5xl font-extrabold tracking-tight">150</span>
+          <span className="ms-1 text-xl font-normal text-gray-500 ">
+            /2 hour session
+          </span>
+        </div>
         <h1 className="font-bold tracking-wider my-2">Session Consists of:</h1>
-        <ul className="text-left list-disc ml-5 my-2">
+        <ul className="text-left list-disc ml-5 my-2 text-gray-700">
           <li>
             Exfoliating the skin with silk gloves to prepare the body to receive
             the oil and stimulate lymph
@@ -36,8 +48,20 @@ function Services() {
         </ul>
         <Button className="py-2 font-semibold my-3 ">Click to Schedule</Button>
       </Box>
-
-      <PricingCard />
+      <div className="p-5 md:mb-5 max-w-screen-lg md:mx-auto border-2 mx-3 md:border-0">
+        <Headline className="text-center">Standard Massage Pricing</Headline>
+        <p className="text-center md:my-5 text-gray-500 my-3">
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nemo
+          repudiandae doloremque reprehenderit a nostrum necessitatibus sapiente
+          voluptatem quibusdam commodi sed neque magnam possimus quas iste
+          quidem, soluta sunt voluptate at.
+        </p>
+        <div className="flex flex-col gap-3 md:gap-0 md:flex-row justify-center items-center">
+          <PricingCard />
+          <PricingCard />
+          <PricingCard />
+        </div>
+      </div>
     </section>
   );
 }
--- src/ui/PricingCard.jsx
+++ src/ui/PricingCard.jsx
@@ -1,20 +1,22 @@
+import Button from "./Button";
+
 function PricingCard() {
   return (
-    <div className="w-full max-w-sm p-4 bg-white border border-gray-200 rounded-lg shadow sm:p-8 dark:bg-gray-800 dark:border-gray-700">
-      <h5 className="mb-4 text-xl font-medium text-gray-500 dark:text-gray-400">
-        Standard plan
+    <div className="w-full max-w-sm p-4 bg-white border border-gray-200 rounded-lg md:shadow sm:p-8 text-center   ">
+      <h5 className="mb-4 text-xl font-medium text-gray-500 ">
+        Standard Massage
       </h5>
-      <div className="flex items-baseline text-gray-900 dark:text-white">
+      <div className="flex items-baseline text-gray-900e">
         <span className="text-3xl font-semibold">$</span>
         <span className="text-5xl font-extrabold tracking-tight">49</span>
-        <span className="ms-1 text-xl font-normal text-gray-500 dark:text-gray-400">
-          /month
+        <span className="ms-1 text-xl font-normal text-gray-500 ">
+          /90 minutes
         </span>
       </div>
       <ul role="list" className="space-y-5 my-7">
         <li className="flex items-center">
           <svg
-            className="flex-shrink-0 w-4 h-4 text-blue-700 dark:text-blue-500"
+            className="flex-shrink-0 w-4 h-4 text-blue-700 "
             aria-hidden="true"
             xmlns="http://www.w3.org/2000/svg"
             fill="currentColor"
@@ -22,13 +24,13 @@ function PricingCard() {
           >
             <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5Zm3.707 8.207-4 4a1 1 0 0 1-1.414 0l-2-2a1 1 0 0 1 1.414-1.414L9 10.586l3.293-3.293a1 1 0 0 1 1.414 1.414Z" />
           </svg>
-          <span className="text-base font-normal leading-tight text-gray-500 dark:text-gray-400 ms-3">
+          <span className="text-base font-normal leading-tight text-gray-500  ms-3">
             2 team members
           </span>
         </li>
         <li className="flex">
           <svg
-            className="flex-shrink-0 w-4 h-4 text-blue-700 dark:text-blue-500"
+            className="flex-shrink-0 w-4 h-4 text-blue-700 "
             aria-hidden="true"
             xmlns="http://www.w3.org/2000/svg"
             fill="currentColor"
@@ -54,7 +56,7 @@ function PricingCard() {
             Integration help
           </span>
         </li>
-        <li className="flex line-through decoration-gray-500">
+        <li className="hidden md:flex line-through decoration-gray-500">
           <svg
             className="flex-shrink-0 w-4 h-4 text-gray-400 dark:text-gray-500"
             aria-hidden="true"
@@ -68,21 +70,7 @@ function PricingCard() {
             Sketch Files
           </span>
         </li>
-        <li className="flex line-through decoration-gray-500">
-          <svg
-            className="flex-shrink-0 w-4 h-4 text-gray-400 dark:text-gray-500"
-            aria-hidden="true"
-            xmlns="http://www.w3.org/2000/svg"
-            fill="currentColor"
-            viewBox="0 0 20 20"
-          >
-            <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5Zm3.707 8.207-4 4a1 1 0 0 1-1.414 0l-2-2a1 1 0 0 1 1.414-1.414L9 10.586l3.293-3.293a1 1 0 0 1 1.414 1.414Z" />
-          </svg>
-          <span className="text-base font-normal leading-tight text-gray-500 ms-3">
-            API Access
-          </span>
-        </li>
-        <li className="flex line-through decoration-gray-500">
+        <li className="hidden md:flex line-through decoration-gray-500">
           <svg
             className="flex-shrink-0 w-4 h-4 text-gray-400 dark:text-gray-500"
             aria-hidden="true"
@@ -96,7 +84,7 @@ function PricingCard() {
             Complete documentation
           </span>
         </li>
-        <li className="flex line-through decoration-gray-500">
+        <li className="hidden md:flex line-through decoration-gray-500">
           <svg
             className="flex-shrink-0 w-4 h-4 text-gray-400 dark:text-gray-500"
             aria-hidden="true"
@@ -111,12 +99,7 @@ function PricingCard() {
           </span>
         </li>
       </ul>
-      <button
-        type="button"
-        className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-200 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-900 font-medium rounded-lg text-sm px-5 py-2.5 inline-flex justify-center w-full text-center"
-      >
-        Choose plan
-      </button>
+      <Button>Schedule Now</Button>
     </div>
   );
 }
