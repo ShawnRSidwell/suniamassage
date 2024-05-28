3d3143d61c29aeba904255e553de9ff98332382d
ShawnRSidwell <shawnrsidwell@gmail.com>
2024-05-28 12:53:27 -0700

    Change ayurvedic massage styling

--- src/pages/Services.jsx
+++ src/pages/Services.jsx
@@ -23,7 +23,7 @@ function Services() {
           not wear any undergarments for the ideal treatment. Of course all of
           this can be modified to suit your comfort level.
         </p>
-        <div className="flex items-baseline text-gray-900 ">
+        <div className="flex items-baseline text-gray-900 my-3 mt-5 ">
           <span className="text-3xl font-semibold">$</span>
           <span className="text-5xl font-extrabold tracking-tight">150</span>
           <span className="ms-1 text-xl font-normal text-gray-500 ">
@@ -31,14 +31,14 @@ function Services() {
           </span>
         </div>
         <h1 className="font-bold tracking-wider my-2">Session Consists of:</h1>
-        <ul className="text-left list-disc ml-5 my-2 text-gray-700">
+        <ul className="text-left list-disc ml-5 my-2 text-gray-700 text-sm md:text-base tracking-tight">
           <li>
             Exfoliating the skin with silk gloves to prepare the body to receive
-            the oil and stimulate lymph
+            the oil and stimulate lymph.
           </li>
           <li>
             Pouring warm oil onto the body and creating friction to remove
-            unbalanced “vata”
+            unbalanced “vata”.
           </li>
           <li>
             Driving the oil deep into the tissue using massage techniques to
