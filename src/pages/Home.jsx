2dab6d9bf86a729ac768a91125943c9e4ff69184
ShawnRSidwell <shawnrsidwell@gmail.com>
2024-08-31 10:36:03 -0700

    Add CallToAction component

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,8 @@
-import Card from "../ui/Card";
+import CallToActionSection from "../ui/CallToActionSection";
 import CardGroup from "../ui/CardGroup";
 import Carousel from "../ui/Carousel";
 
 import Jumbotron from "../ui/Jumbotron";
-import Panel from "../ui/Panel";
 
 function Home() {
   return (
@@ -11,22 +10,7 @@ function Home() {
       <Jumbotron />
       <CardGroup />
       <Carousel />
-      <div className="my-6 relative flex flex-col justify-center items-center md:py-24 md:my-12">
-        {/* <hr className="border-[#60A76F] w-5/6 md:hidden" /> */}
-        <img
-          src="blue-flowers-oils-above.jpg"
-          alt="blue flowers and oil"
-          className="hidden md:block absolute inset-0 w-full h-full object-cover z-0 opacity-50 "
-        />
-        <Panel
-          headline="Take the next step to self-care here"
-          buttonText="Schedule Appointment"
-          className="mb-5 relative z-10"
-        >
-          <p>For Consultations, Body Therapies and Energetic Coaching</p>
-        </Panel>
-        <hr className="border-cGreen-400 w-5/6 md:hidden" />
-      </div>
+      <CallToActionSection />
     </section>
   );
 }
---
+++ src/ui/CallToActionSection.jsx
@@ -0,0 +1,24 @@
+import Panel from "./Panel";
+
+function CallToActionSection() {
+  return (
+    <div className="my-6 relative flex flex-col justify-center items-center md:py-24 md:my-12">
+      {/* <hr className="border-[#60A76F] w-5/6 md:hidden" /> */}
+      <img
+        src="blue-flowers-oils-above.jpg"
+        alt="blue flowers and oil"
+        className="hidden md:block absolute inset-0 w-full h-full object-cover z-0 opacity-50 "
+      />
+      <Panel
+        headline="Take the next step to self-care here"
+        buttonText="Schedule Appointment"
+        className="mb-5 relative z-10"
+      >
+        <p>For Consultations, Body Therapies and Energetic Coaching</p>
+      </Panel>
+      <hr className="border-cGreen-400 w-5/6 md:hidden" />;
+    </div>
+  );
+}
+
+export default CallToActionSection;
