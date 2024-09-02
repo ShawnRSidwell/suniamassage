c3949bd5983d871593cb7c15729a8549dc450ab0
ShawnRSidwell <shawnrsidwell@gmail.com>
2024-09-02 12:36:49 -0700

    change jumbotron styling

--- src/ui/Jumbotron.jsx
+++ src/ui/Jumbotron.jsx
@@ -5,7 +5,7 @@ function Jumbotron() {
     <section className="bg-cover bg-no-repeat bg-[url('/hand-massage.jpg')] bg-top flex flex-col">
       <div className="text-center w-full h-[75dvh] flex justify-center items-center">
         <div className="bg-cBeige-400 w-3/4 rounded-3xl p-6 bg-opacity-85 shadow-lg">
-          <h1 className="mb-4 text-4xl font-extrabold tracking-tight leading-none text-cGreen-500 md:text-5xl lg:text-6xl">
+          <h1 className="mb-4 text-4xl font-extrabold tracking-tight leading-none text-cPurple-500 md:text-5xl lg:text-6xl">
             Revitalize Your Soul
           </h1>
 
