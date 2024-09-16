7ba0b350156dc4145c3a56f30be80510a0da640e
ShawnRSidwell <shawnrsidwell@gmail.com>
2024-09-16 14:03:39 -0700

    Change approach section

--- src/Features/about/ApproachCard.jsx
+++ src/Features/about/ApproachCard.jsx
@@ -9,15 +9,17 @@ function ApproachCard() {
         alt="Large massage picture"
         reverse={true}
       >
-        <p className="w-11/12 py-2 md:py-5 text-gray-700">
+        <p className="w-11/12 py-2 text-gray-700">
           The Ayurvedic body treatment (Garshana/Abhyanga/Vishesh) is an ancient
           practice that originated in India as a purification process. The flow
           consists of a head to toe body “polishing” which includes the scalp,
           face and stomach. The intention is not to focus on specific areas of
           tension in the body, rather it is a holistic approach. The benefits
           include stress reduction, improved circulation, sleep, detoxification,
-          and creating an overall feeling of serenity. Each personally
-          customized session begins with Garshana, where I use silk gloves to
+          and creating an overall feeling of serenity.
+        </p>
+        <p className="w-11/12 py-2 text-gray-700">
+          Each treatment begins with Garshana, where I use silk gloves to
           exfoliate and prepare the skin for the oil while stimulating the
           lymphatic system. Next, Abhyanga includes applying a generous amount
           of warm, herbal-infused oil onto the skin then rhythmically massaging
@@ -28,18 +30,12 @@ function ApproachCard() {
           deeper massage techniques, Vishesh, to invite further absorption of
           the oil into the tissues, facilitating healing at a cellular level.
         </p>
-        <p className="w-11/12 py-2 md:py-5 text-gray-700">
-          Each personally customized session begins with Garshana, where I use
-          silk gloves to exfoliate and prepare the skin for the oil while
-          stimulating the lymphatic system. Next, Abhyanga includes applying a
-          generous amount of warm, herbal-infused oil onto the skin then
-          rhythmically massaging with gentle friction to encourage absorption.
-          The long strokes along the bones and circular attention to joints
-          invite unbalanced energy to exit the body. The third part begins with
-          placing hot towels on the skin, to encourage deeper penetration of the
-          oil. Lastly, I implement deeper massage techniques, Vishesh, to invite
-          further absorption of the oil into the tissues, facilitating healing
-          at a cellular level.
+        <p className="w-11/12 py-2 text-gray-700">
+          Each session is personally customized with specific organic
+          herbal-infused oils depending on what the client is seeking harmony
+          and balance for. The client will guide the intention of each session
+          based on their particular needs. The treatment is mostly silent with
+          soft music in the background to encourage the client to focus within.
         </p>
       </FeatureCard>
     </section>
